dc1815b120bd8f8dab43ea510613b4c0927c0a1f
Mark Lister <Mark.Lister@callcreditgroup.com>
2019-09-24 17:05:52 +0100

    Add a couple more skeleton methods

--- GraduateLINQWorkshop/LINQProblems/ConvertToLinq.cs
+++ GraduateLINQWorkshop/LINQProblems/ConvertToLinq.cs
@@ -81,5 +81,25 @@ namespace LINQProblems
         {
             throw new NotImplementedException();
         }
+
+        public static IList<Colour> FindAllFavouriteColours(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IList<string> FindFirstNamesWhichExistInBothLists(IEnumerable<Person> people, IEnumerable<Person> otherPeople)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IDictionary<string, Colour> CreateDictionaryOfNameToFavouriteColour(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int FindTotalAgeOfAllPeople(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

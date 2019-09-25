d715b6ccb8bc6944e661e51313b1e703a88dadcd
Mark Lister <Mark.Lister@callcreditgroup.com>
2019-09-25 14:44:06 +0100

    Create 3 levels of statements

---
+++ GraduateLINQWorkshop/LINQProblems/BasicLinq.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQProblems
+{
+    public static class BasicLinq
+    {
+        public static List<string> GetNamesOfPeople(List<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Person> GetPeopleOlderThan(List<Person> people, int age)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int CalculateNumberOfPeople(List<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IncludesPersonWithName(List<Person> people, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Person> OrderByAgeAscending(List<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Person> OrderByAgeDescdending(List<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Person FindOldestPerson(List<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GraduateLINQWorkshop/LINQProblems/IntermediateLinq.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQProblems
+{
+    public static class IntermediateLinq
+    {
+        public static List<Person> CombinePeople(List<Person> people, List<Person> otherPeople)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<string> NamesOfPeopleOlderThan(List<Person> people, int age)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int CalculateHowManyPeopleLiveIn(List<Person> people, string birthplace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IList<Person> FindPeopleWhosFavouriteColourIs(IEnumerable<Person> people, Colour favouriteColour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool DoesSomeoneExistWhoIsOverAgeWithFavouriteColourOf(IEnumerable<Person> people, int age, Colour favouriteColour)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IList<Person> PeopleWithOtherNames(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Person> PeopleWithOtherNameOf(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

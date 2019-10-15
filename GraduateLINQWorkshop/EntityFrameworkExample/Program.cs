7d2b7062152b864d0211826876f4e0f8b5688472
Mark Lister <Mark.Lister@callcreditgroup.com>
2019-10-15 16:29:24 +0100

    Northwind EF example

---
+++ GraduateLINQWorkshop/EntityFrameworkExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var database = new northwndEntities())
+            {
+                var products = database.Products.Where(product => product.UnitPrice > 50);
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.ProductName + " : " + product.UnitPrice);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GraduateLINQWorkshop/EntityFrameworkExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EntityFrameworkExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Callcredit Information Group")]
+[assembly: AssemblyProduct("EntityFrameworkExample")]
+[assembly: AssemblyCopyright("Copyright © Callcredit Information Group 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("692e5027-2d67-43e3-ac87-eb91edde08f3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

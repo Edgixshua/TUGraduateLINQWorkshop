bd680277e5dbb901e59e67b6de7e851594f4716c
Mark Lister <Mark.Lister@callcreditgroup.com>
2019-10-15 11:39:01 +0100

    Add tests around total age of all people

---
+++ GraduateLINQWorkshop/LINQProblems.Tests/AdvancedLinqTests/FindTotalAgeOfAllPeopleTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace LINQProblems.Tests.AdvancedLinqTests
+{
+    [TestFixture]
+    public class FindTotalAgeOfAllPeopleTests
+    {
+        [Test]
+        public void FindTotalAgeOfAllPeople_EmptyList_Returns0()
+        {
+            // Arrange 
+            var people = TestData.NoPeople;
+
+            // Act
+            var totalAge = AdvancedLinq.FindTotalAgeOfAllPeople(people);
+
+            // Assert
+            totalAge.Should().Be(0);
+        }
+
+        [Test]
+        public void FindTotalAgeOfAllPeople_AbbySmith_Returns29()
+        {
+            // Arrange 
+            var people = TestData.AbbySmith;
+
+            // Act
+            var totalAge = AdvancedLinq.FindTotalAgeOfAllPeople(people);
+
+            // Assert
+            totalAge.Should().Be(29);
+        }
+
+        [Test]
+        public void FindTotalAgeOfAllPeople_Everyone_Returns403()
+        {
+            // Arrange 
+            var people = TestData.LotsOfPeople;
+
+            // Act
+            var totalAge = AdvancedLinq.FindTotalAgeOfAllPeople(people);
+
+            // Assert
+            totalAge.Should().Be(403);
+        }
+    }
+}

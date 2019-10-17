9b7c69e7507afdfe5e6243d3ea9ee8fe58a0a9d7
Daniel Pickles <Daniel.Pickles@callcreditgroup.com>
2019-10-17 09:37:28 +0100

    Workshop 1

--- GraduateLINQWorkshop/LINQProblems/BasicLinq.cs
+++ GraduateLINQWorkshop/LINQProblems/BasicLinq.cs
@@ -8,34 +8,39 @@ namespace LINQProblems
     {
         public static List<string> GetFirstNamesOfPeople(List<Person> people)
         {
-            throw new NotImplementedException();
+            return (from p in people
+                    select p.FirstName).ToList<string>();
         }
 
         // This is an inclusive older than, e.g. someone with an age of 18 counts as being older
         // than a specified age of 18
         public static List<Person> GetPeopleOlderThan(List<Person> people, int age)
         {
-            throw new NotImplementedException();
+            return (from p in people
+                    where p.Age >= age
+                    select p).ToList<Person>();
         }
 
         public static bool IncludesPersonWithName(List<Person> people, string name)
         {
-            throw new NotImplementedException();
+            return people.Any(p => p.FirstName == name);
         }
 
         public static List<Person> OrderByAgeAscending(List<Person> people)
         {
-            throw new NotImplementedException();
+            return (from p in people
+                    orderby p.Age ascending
+                    select p).ToList<Person>();
         }
 
         public static List<Person> OrderByAgeDescending(List<Person> people)
         {
-            throw new NotImplementedException();
+            return people.OrderByDescending(p => p.Age).ToList();
         }
 
         public static Person FindOldestPerson(List<Person> people)
         {
-            throw new NotImplementedException();
+            return people.OrderByDescending(p => p.Age).FirstOrDefault();
         }
     }
 }
--- GraduateLINQWorkshop/LINQProblems/IntermediateLinq.cs
+++ GraduateLINQWorkshop/LINQProblems/IntermediateLinq.cs
@@ -8,41 +8,45 @@ namespace LINQProblems
     {
         public static List<Person> CombinePeople(List<Person> people, List<Person> otherPeople)
         {
-            throw new NotImplementedException();
+            return people.Concat(otherPeople).ToList();
         }
 
         public static List<string> NamesOfPeopleOlderThan(List<Person> people, int age)
         {
-            throw new NotImplementedException();
+            return (from p in people
+                    where p.Age >= age
+                    select p.FirstName).ToList();
         }
 
 
         public static int CalculateHowManyPeopleLiveIn(List<Person> people, string birthplace)
         {
-            throw new NotImplementedException();
+            return people.Count(p => p.PlaceOfBirth == birthplace);
         }
 
 
         public static IList<Person> FindPeopleWhosFavouriteColourIs(IEnumerable<Person> people, Colour favouriteColour)
         {
-            throw new NotImplementedException();
+            return (from p in people
+                    where p.FavouriteColour == favouriteColour
+                    select p).ToList();
         }
 
 
         public static bool DoesSomeoneExistWhoIsOverAgeWithFavouriteColourOf(IEnumerable<Person> people, int age, Colour favouriteColour)
         {
-            throw new NotImplementedException();
+            return people.Any(p => p.Age > age && p.FavouriteColour == favouriteColour);
         }
 
 
         public static IList<Person> PeopleWithOtherNames(IEnumerable<Person> people)
         {
-            throw new NotImplementedException();
+            return people.Where(p => p.OtherNames != null && p.OtherNames.Count > 0).ToList();
         }
 
         public static IEnumerable<Person> PeopleWithOtherNameOf(IEnumerable<Person> people, string name)
         {
-            throw new NotImplementedException();
+            return people.Where(p => p.OtherNames != null && p.OtherNames.Count > 0 && p.OtherNames.Contains(name));
         }
     }
 }
